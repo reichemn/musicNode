74217f4006d8ccc475227331e3fae13e57bbf505
Max <st143430@stud.uni-stuttgart.de>
2017-04-05 00:52:15 +0200

    Filter für die Suche überarbeitet.

--- app/app.js
+++ app/app.js
@@ -1,6 +1,9 @@
 'use strict';
 
 // Declare app level module which depends on views, and components
+
+
+
 angular.module('myApp', [
   'ngRoute',
   'myApp.view1',
--- app/view1/view1.js
+++ app/view1/view1.js
@@ -9,7 +9,7 @@ angular.module('myApp.view1', ['ngRoute'])
   });
 }])
 
-.controller('View1Ctrl', ['$scope','$http', function(scope, http) {
+.controller('View1Ctrl', ['$scope','$http', '$filter', function(scope, http, filter) {
     var song1 = new Song(0,"test title","test interpret","test album",new SongImage(0,"http://kingofwallpapers.com/song/song-010.jpg","https://encrypted-tbn3.gstatic.com/images?q=tbn:ANd9GcR8x47T7Nr_A7F_tos3oKOB3K4DwzFD7ZcKizWmLMPIikNB56in4g"),null);
     var song2 = new Song(1,"Song 2","zweiter interpret","zweites album",new SongImage(1,null,null),null);
     var song3 = new Song(2,"Song 3","zweiter interpret","zweites album",new SongImage(2,"http://kingofwallpapers.com/song/song-001.jpg","https://encrypted-tbn3.gstatic.com/images?q=tbn:ANd9GcT8_SonIBak5_M_5V-IN6MSA2YRJR78X7pvw4hyVYioVwRKtaU-ew"),null);
@@ -19,27 +19,29 @@ angular.module('myApp.view1', ['ngRoute'])
       // Todo: Server kommunikation
   }
 
+  // Filter fuer die Songsuche
+  scope.filteredSongs = filter('filter')(scope.allSongs, scope.queryInput);
   scope.selectedFilter="all";
   scope.changeFilter = function () {
-    //Todo: Suche sucht aktuell auch in der Url zum Bild, das sollte behoben werden.
-    scope.query = {
-      "title" : "",
-      "artist" : "",
-      "album" : ""
-    };
-      switch(scope.selectedFilter){
-          case "all":
-              scope.query= scope.queryInput;
-            break;
-          case "title":
-            scope.query.title = scope.queryInput;
-            break;
-          case "album":
-              scope.query.album = scope.queryInput;
-            break;
-          case "artist":
-              scope.query.artist = scope.queryInput;
-            break;
-      }
+
+      var songFilterFunction = function(song) {
+
+          switch(scope.selectedFilter){
+              case "all":
+                  return song.title.indexOf(scope.queryInput || '') !== -1 || song.artist.indexOf(scope.queryInput || '') !== -1 || song.album.indexOf(scope.queryInput || '') !== -1;
+                  break;
+              case "title":
+                  return song.title.indexOf(scope.queryInput || '') !== -1;
+                  break;
+              case "album":
+                  return song.album.indexOf(scope.queryInput || '') !== -1;
+                  break;
+              case "artist":
+                  return song.artist.indexOf(scope.queryInput || '') !== -1;
+                  break;
+          }
+      };
+
+      scope.filteredSongs = filter('filter')(scope.allSongs, songFilterFunction,  scope.queryInput);
   }
 }]);
